849ff56db028315e25087f9dde244480d21fd3a3
Joao Sousa <joaocsousa@users.noreply.github.com>
2026-02-14 23:14:00 +0000

    Attempt to fix jitpack (#8)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
+
 buildscript {
     repositories {
         mavenCentral()
         google()
         gradlePluginPortal()
-        maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev" )
+        maven(url = "https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 
     dependencies {
@@ -16,3 +19,7 @@ plugins {
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.13.0"
 }
 
+// Fix for JitPack GLIBC incompatibility with newer Node.js versions
+rootProject.plugins.withType<NodeJsRootPlugin> {
+    rootProject.the<NodeJsRootExtension>().nodeVersion = "16.13.0"
+}
